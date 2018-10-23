14c0f76176fab53a88a693ee4c3d44d336362d94
raj59595 <34572070+raj59595@users.noreply.github.com>
2018-10-24 09:28:49 +1100

    The robot entity with position selection.

---
+++ Robot.js
@@ -0,0 +1,149 @@
+var clc = require('cli-color');
+
+
+var Robot = function() {
+  this.isPlaced = false;
+  this.position = {
+    x: null,
+    y: null
+  };
+  this.direction = null;
+};
+/*
+directions as required.
+ */
+
+var directionMap = {
+  north: {
+    value: 'north',
+    left: 'west',
+    right: 'east'
+  },
+  east: {
+    value: 'east',
+    left: 'north',
+    right: 'south'
+  },
+  south: {
+    value: 'south',
+    left: 'east',
+    right: 'west'
+  },
+  west: {
+    value: 'west',
+    left: 'south',
+    right: 'north'
+  }
+};
+
+
+Robot.prototype.tableSize = {x: 4, y: 4};
+
+
+Robot.prototype.place = function(paramList) {
+  var x = paramList[0];
+  var y = paramList[1];
+  var direction = directionMap[paramList[2]].value;
+
+  // Ignore if placement is off the table
+  if (x > this.tableSize.x || y > this.tableSize.y) {
+    return this;
+  }
+
+  // Modify robot with directions
+  this.isPlaced = true;
+  this.position.x = x;
+  this.position.y = y;
+  this.direction = direction;
+
+  return this;
+};
+
+
+Robot.prototype.move = function() {
+  // Ignore command if robot is not yet placed
+  if (!this.isPlaced) {
+    return this;
+  }
+
+  var x = this.position.x;
+  var y = this.position.y;
+
+  switch (this.direction) {
+    case 'north':
+      if (++y < this.tableSize.y) {
+        this.position = {x: x, y: y}
+      }
+      break;
+    case 'east':
+      if (++x < this.tableSize.x) {
+        this.position = {x: x, y: y}
+      }
+      break;
+    case 'south':
+      if (--y >= 0) {
+        this.position = {x: x, y: y};
+      }
+      break;
+    case 'west':
+      if (--x >= 0) {
+        this.position = {x: x, y: y}
+      }
+      break;
+    default:
+      break;
+  }
+
+  return this;
+};
+
+
+Robot.prototype.turn = function(direction) {
+  // Ignore command if robot is not yet placed
+  if (!this.isPlaced) {
+    return this;
+  }
+
+  var resultDirection = directionMap[this.direction][direction];
+
+  if (resultDirection) {
+    this.direction = resultDirection;
+  }
+
+  return this;
+};
+
+
+Robot.prototype.report = function() {
+  // Ignore command if robot is not yet placed
+  if (!this.isPlaced) {
+    return this;
+  }
+
+  console.log(clc.blue('REPORT:') + ' ' + [this.position.x, this.position.y,this.direction.toUpperCase()].join(','));
+
+  return this;
+};
+
+
+Robot.prototype.runInstructions = function(instructionList) {
+  var instruction;
+  var robot = this;
+
+  // Run each instruction in series
+  for (var i = 0; i<instructionList.length; i++) {
+    instruction = instructionList[i];
+    if (instruction.args) {
+      robot = this[instruction.command](instruction.args);
+    } else {
+      robot = this[instruction.command]()
+    }
+  }
+
+  return robot;
+};
+
+module.exports = Robot;
+
+
+
